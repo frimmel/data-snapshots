edfc4805e96ae885b60fe7e591741f211f70eccf
Mark Phillips <embeepea@git>
2013-11-13 12:21:04 -0500

    remove another hardcoded reference to undefined content_taxonomy_allowed_values

---
+++ foo.php
@@ -0,0 +1,281 @@
+array(
+  (object) array(
+      'vid' => '1',
+      'uid' => '1',
+      'title' => 'US Drought Monitor',
+      'log' => '',
+      'status' => '1',
+      'comment' => '2',
+      'promote' => '1',
+      'sticky' => '0',
+      'vuuid' => '837a158b-57e2-4448-a881-e851cbcc2b21',
+      'nid' => '1',
+      'type' => 'data_snapshot_data_source',
+      'language' => 'und',
+      'created' => '1384361912',
+      'changed' => '1384361912',
+      'tnid' => '0',
+      'translate' => '0',
+      'uuid' => 'df42f9a0-2edc-4826-b04d-6b3fd0f6df04',
+      'revision_timestamp' => '1384361912',
+      'revision_uid' => '1',
+      'field_climate_gov_section' => array(
+        'und' => array(
+          array(
+            'tid' => '6',
+          ),
+        ),
+      ),
+      'field_dssds_catalog_url' => array(
+        'und' => array(
+          array(
+            'url' => 'http://www.dcurl.org/this/is/the/link',
+            'title' => 'this is the dcrurl title ',
+            'attributes' => array(),
+          ),
+        ),
+      ),
+      'field_dssds_colors' => array(
+        'und' => array(
+          array(
+            'value' => 'The colors are really sweet.',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>The colors are really sweet.</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_data_access_url' => array(
+        'und' => array(
+          array(
+            'url' => 'http://www.data.com/access/url/goes/here',
+            'title' => 'data access url title here',
+            'attributes' => array(),
+          ),
+        ),
+      ),
+      'field_dssds_data_format' => array(
+        'und' => array(
+          array(
+            'value' => 'data format description',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>data format description</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_data_matters' => array(
+        'und' => array(
+          array(
+            'value' => "It matters because it's important!",
+            'format' => 'filtered_html',
+            'safe_value' => "<p>It matters because it's important!</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_data_provider' => array(
+        'und' => array(
+          array(
+            'url' => 'http://www.herrprofessordocktor.com',
+            'title' => 'Herr Professor Docktor',
+            'attributes' => array(),
+          ),
+        ),
+      ),
+      'field_dssds_description_auth' => array(
+        'und' => array(
+          array(
+            'value' => 'Description Author name goes here',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>Description Author name goes here</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_editorial_commen' => array(
+        'und' => array(
+          array(
+            'value' => 'More editorial comments here!',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>More editorial comments here!</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_english_descript' => array(
+        'und' => array(
+          array(
+            'value' => "It's when things are really dry, stupid.",
+            'format' => 'filtered_html',
+            'safe_value' => "<p>It's when things are really dry, stupid.</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_framing_question' => array(
+        'und' => array(
+          array(
+            'value' => 'What is drought?',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>What is drought?</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_image_frequency' => array(
+        'und' => array(
+          array(
+            'tid' => '1',
+          ),
+        ),
+      ),
+      'field_dssds_official_name' => array(
+        'und' => array(
+          array(
+            'value' => 'USDM-234-223542-2',
+            'format' => NULL,
+            'safe_value' => 'USDM-234-223542-2',
+          ),
+        ),
+      ),
+      'field_dssds_plain_english_an' => array(
+        'und' => array(
+          array(
+            'value' => "It's a kind of beer.  Oh no wait that's draught.",
+            'format' => 'filtered_html',
+            'safe_value' => "<p>It's a kind of beer.  Oh no wait that's draught.</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_processing_envir' => array(
+        'und' => array(
+          array(
+            'value' => 'And a description of the processing environment.',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>And a description of the processing environment.</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_processing_steps' => array(
+        'und' => array(
+          array(
+            'value' => 'This is an outline of the processing steps',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>This is an outline of the processing steps</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_references' => array(
+        'und' => array(
+          array(
+            'value' => 'Some references go here.',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>Some references go here.</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_reviewer' => array(
+        'und' => array(
+          array(
+            'value' => 'Reviewer name goes here',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>Reviewer name goes here</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_reviewer_affilia' => array(
+        'und' => array(
+          array(
+            'value' => 'Affiliation of Reviewer goes here',
+            'format' => NULL,
+            'safe_value' => 'Affiliation of Reviewer goes here',
+          ),
+        ),
+      ),
+      'field_dssds_secondary_questi' => array(
+        'und' => array(
+          array(
+            'value' => 'What is drought, really?',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>What is drought, really?</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_technical_commen' => array(
+        'und' => array(
+          array(
+            'value' => 'Here come some additional technical comments.',
+            'format' => 'filtered_html',
+            'safe_value' => "<p>Here come some additional technical comments.</p>\n",
+          ),
+        ),
+      ),
+      'field_dssds_themes' => array(
+        'und' => array(
+          array(
+            'tid' => '3',
+          ),
+        ),
+      ),
+      'rdf_mapping' => array(
+        'rdftype' => array(
+          'sioc:Item',
+          'foaf:Document',
+        ),
+        'title' => array(
+          'predicates' => array(
+            'dc:title',
+          ),
+        ),
+        'created' => array(
+          'predicates' => array(
+            'dc:date',
+            'dc:created',
+          ),
+          'datatype' => 'xsd:dateTime',
+          'callback' => 'date_iso8601',
+        ),
+        'changed' => array(
+          'predicates' => array(
+            'dc:modified',
+          ),
+          'datatype' => 'xsd:dateTime',
+          'callback' => 'date_iso8601',
+        ),
+        'body' => array(
+          'predicates' => array(
+            'content:encoded',
+          ),
+        ),
+        'uid' => array(
+          'predicates' => array(
+            'sioc:has_creator',
+          ),
+          'type' => 'rel',
+        ),
+        'name' => array(
+          'predicates' => array(
+            'foaf:name',
+          ),
+        ),
+        'comment_count' => array(
+          'predicates' => array(
+            'sioc:num_replies',
+          ),
+          'datatype' => 'xsd:integer',
+        ),
+        'last_activity' => array(
+          'predicates' => array(
+            'sioc:last_activity_date',
+          ),
+          'datatype' => 'xsd:dateTime',
+          'callback' => 'date_iso8601',
+        ),
+      ),
+      'cid' => '0',
+      'last_comment_timestamp' => '1384361912',
+      'last_comment_name' => NULL,
+      'last_comment_uid' => '1',
+      'comment_count' => '0',
+      'name' => 'admin',
+      'picture' => '0',
+      'data' => 'b:0;',
+      'path' => FALSE,
+      'menu' => NULL,
+      'node_export_drupal_version' => '7',
+    ),
+)
